7f4ba981551e5bb1f6bae277c56249539d2d1866
Jackson Graziano <jackgraziano@gmail.com>
2017-06-12 11:01:41 -0300

    style

--- ons.rb
+++ ons.rb
@@ -1,16 +1,23 @@
 require 'open-uri'
 require 'nokogiri'
 
+# Consumo to SNE
 url_consumo = "http://www.ons.org.br/home/grafico.asp"
 doc_consumo = Nokogiri::HTML(open(url_consumo))
-consumo = doc_consumo.css("b").children.text.split(" ")[0]
-puts consumo
+consumo = [ doc_consumo.css("b").children.text.split(" ")[0],
+            doc_consumo.css("#data").text.split(" ")[1]
+          ]
 
+# Nivel dos reservatorios
 url_reserva = "http://www.ons.org.br/home/reservatorio.asp"
 doc_reserva = Nokogiri::HTML(open(url_reserva)).xpath('//table/tr')
 reservas = []
-reservas << [doc_reserva.xpath('//tr[2]/td[1]').text, doc_reserva  .xpath('//tr[2]/td[2]').text]
-reservas << [doc_reserva.xpath('//tr[3]/td[1]').text, doc_reserva  .xpath('//tr[3]/td[2]').text]
-reservas << [doc_reserva.xpath('//tr[4]/td[1]').text, doc_reserva  .xpath('//tr[4]/td[2]').text]
-reservas << [doc_reserva.xpath('//tr[5]/td[1]').text, doc_reserva  .xpath('//tr[5]/td[2]').text]
+(2..5).each do |n|
+  reservas << [ doc_reserva.xpath("//tr[#{n}]/td[1]").text,
+                doc_reserva.xpath("//tr[#{n}]/td[2]").text
+              ]
+end
+
+# print
+p consumo
 p reservas
